f2e5ea6915a8e2fc33de1f9bff85bdd3e7d7f208
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-28 16:53:26 +0500

    Shared project extended

---
+++ src/Common/Shared/ExceptionAbstractions/ExceptionStatusCode.cs
@@ -0,0 +1,42 @@
+﻿/* 
+	BookStore
+	Copyright (c) 2024, Sharifjon Abdulloev.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 3.0, 
+	as published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License, version 3.0, for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Domain.Primitives;
+
+namespace Shared.ExceptionAbstractions
+{
+	/// <summary>
+	/// Represents a base enumeration class for exception status codes enums.
+	/// </summary>
+	/// <inheritdoc/>
+	public abstract class ExceptionStatusCode : Enumeration<ExceptionStatusCode, string>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ExceptionStatusCode"/> class with a name and value.
+		/// </summary>
+		/// <inheritdoc/>
+		protected ExceptionStatusCode(string name, string value) : base(name, value)
+		{
+		}
+
+		/// <inheritdoc/>
+		protected sealed override bool IsValueEqual(string? first, string? second)
+		{
+			return first == second;
+		}
+	}
+}
