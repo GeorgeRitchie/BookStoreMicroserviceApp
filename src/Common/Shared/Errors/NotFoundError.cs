b67b84803bd786f320f9ad339c23d5f26bdcfb61
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-06-28 14:15:02 +0500

    Shared code done in Shared project

---
+++ src/Common/Shared/Errors/NotFoundError.cs
@@ -0,0 +1,35 @@
+﻿/* 
+	BookStore
+	Copyright (c) 2024, Sharifjon Abdulloev.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 3.0, 
+	as published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License, version 3.0, for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Shared.Errors
+{
+	/// <summary>
+	/// Represents the not found error.
+	/// </summary>
+	public sealed class NotFoundError : Error
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NotFoundError"/> class.
+		/// </summary>
+		/// <param name="code">The error code.</param>
+		/// <param name="message">The error message.</param>
+		public NotFoundError(string code, string message)
+			: base(code, message)
+		{
+		}
+	}
+}
---
+++ src/Common/Shared/Results/ValidationResult.cs
@@ -0,0 +1,50 @@
+﻿/* 
+	BookStore
+	Copyright (c) 2024, Sharifjon Abdulloev.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 3.0, 
+	as published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License, version 3.0, for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Shared.Errors;
+
+namespace Shared.Results
+{
+	/// <summary>
+	/// Represents a validation result of some operation, with the errors that occurred.
+	/// </summary>
+	public sealed class ValidationResult : Result, IValidationResult
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ValidationResult"/> class.
+		/// </summary>
+		/// <param name="errors">The errors.</param>
+		private ValidationResult(Error[] errors)
+			: base(false, errors)
+		{
+		}
+
+		/// <summary>
+		/// Creates a new <see cref="ValidationResult"/> with the specified errors.
+		/// </summary>
+		/// <param name="errors">The errors.</param>
+		/// <returns>The new validation result instance with the specified errors.</returns>
+		public static ValidationResult WithErrors(Error[] errors) => new(errors);
+
+		/// <summary>
+		/// Creates a new <see cref="ValidationResult{TValue}"/> with the specified errors.
+		/// </summary>
+		/// <param name="errors">The errors.</param>
+		/// <returns>The new validation result instance with the specified errors.</returns>
+		public static ValidationResult<TValue> WithErrors<TValue>(Error[] errors) => new(errors);
+	}
+}
