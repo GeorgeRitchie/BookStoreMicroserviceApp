0c2c74b82e8b0d2cfc136e3b2aaeebc7951327c1
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-07-24 14:46:51 +0500

    Get book by id done

---
+++ src/backend/CatalogWrite/Service.CatalogWrite.Application/Books/Queries/GetBookById/BookDto.cs
@@ -0,0 +1,177 @@
+﻿/* 
+	BookStore
+	Copyright (c) 2024, Sharifjon Abdulloev.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 3.0, 
+	as published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License, version 3.0, for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Service.CatalogWrite.Application.Common.Models;
+using Service.CatalogWrite.Domain.Authors;
+using Service.CatalogWrite.Domain.Books;
+using Service.CatalogWrite.Domain.Categories;
+using Service.CatalogWrite.Domain.Publishers;
+
+namespace Service.CatalogWrite.Application.Books.Queries.GetBookById
+{
+	/// <summary>
+	/// Represents the dto for <see cref="Book"/> entity.
+	/// </summary>
+	public sealed class BookDto : IMapWith<Book>
+	{
+		/// <summary>
+		/// Book title.
+		/// </summary>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// Book description.
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Book ISBN if available.
+		/// </summary>
+		public string? ISBN { get; set; }
+
+		/// <summary>
+		/// Book language in ISO 639-1 format (e. g. ru, en, fr).
+		/// </summary>
+		public string Language { get; set; }
+
+		/// <summary>
+		/// Book age rating.
+		/// </summary>
+		public uint AgeRating { get; set; }
+
+		/// <summary>
+		/// Book published date if available.
+		/// </summary>
+		public DateOnly? PublishedDate { get; set; }
+
+		/// <summary>
+		/// Book publisher information if available.
+		/// </summary>
+		public PublisherDto? Publisher { get; set; }
+
+		/// <summary>
+		/// Book images.
+		/// </summary>
+		public List<ImageSourceDto> Images { get; set; }
+
+		/// <summary>
+		/// Book authors information.
+		/// </summary>
+		public List<AuthorDto> Authors { get; set; }
+
+		/// <summary>
+		/// Book categories.
+		/// </summary>
+		public List<CategoryDto> Categories { get; set; }
+
+		/// <summary>
+		/// Book source information.
+		/// </summary>
+		public List<BookSourceDto> Sources { get; set; }
+	}
+
+	/// <summary>
+	/// Represents the dto for <see cref="BookSource"/> entity.
+	/// </summary>
+	public class BookSourceDto : IMapWith<BookSource>
+	{
+		/// <summary>
+		/// Book source identifier.
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Book source format (e. g. paper, pdf, txt).
+		/// </summary>
+		public string Format { get; set; }
+
+		/// <summary>
+		/// Book's preview source url of if available.
+		/// </summary>
+		public string? PreviewUrl { get; set; }
+
+		public void Mapping(Profile profile)
+			=> profile.CreateMap<BookSource, BookSourceDto>()
+				.ForMember(dto => dto.Id, opt => opt.MapFrom(c => c.Id.Value))
+				.ForMember(dto => dto.Format, opt => opt.MapFrom(c => c.Format.Name));
+	}
+
+	/// <summary>
+	/// Represents the dto for <see cref="Category"/> entity.
+	/// </summary>
+	public class CategoryDto : IMapWith<Category>
+	{
+		/// <summary>
+		/// Category identifier.
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Category title.
+		/// </summary>
+		public string Title { get; set; }
+
+		public void Mapping(Profile profile)
+			=> profile.CreateMap<Category, CategoryDto>()
+				.ForMember(dto => dto.Id, opt => opt.MapFrom(c => c.Id.Value));
+	}
+
+	/// <summary>
+	/// Represents the dto for <see cref="Author"/> entity.
+	/// </summary>
+	public sealed class AuthorDto : IMapWith<Author>
+	{
+		/// <summary>
+		/// Author identifier.
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Author first name.
+		/// </summary>
+		public string FirstName { get; set; }
+
+		/// <summary>
+		/// Author last name.
+		/// </summary>
+		public string LastName { get; set; }
+
+		public void Mapping(Profile profile)
+			=> profile.CreateMap<Author, AuthorDto>()
+				.ForMember(dto => dto.Id, opt => opt.MapFrom(c => c.Id.Value));
+	}
+
+	/// <summary>
+	/// Represents the dto for <see cref="Publisher"/> entity.
+	/// </summary>
+	public sealed class PublisherDto : IMapWith<Publisher>
+	{
+		/// <summary>
+		/// Publisher identifier.
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Publisher name.
+		/// </summary>
+		public string Name { get; set; }
+
+		public void Mapping(Profile profile)
+			=> profile.CreateMap<Publisher, PublisherDto>()
+				.ForMember(dto => dto.Id, opt => opt.MapFrom(c => c.Id.Value));
+	}
+}
--- src/backend/CatalogWrite/Service.CatalogWrite.Endpoints/Endpoints/Books/GetBookByIdEndpoint.cs
+++ src/backend/CatalogWrite/Service.CatalogWrite.Endpoints/Endpoints/Books/GetBookByIdEndpoint.cs
@@ -15,8 +15,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-using Service.CatalogWrite.Application.Authors.Queries.GetAuthorById;
-using Service.CatalogWrite.Domain.Authors;
+using Service.CatalogWrite.Application.Books.Queries.GetBookById;
 using Service.CatalogWrite.Domain.Books;
 
 namespace Service.CatalogWrite.Endpoints.Endpoints.Books
@@ -30,21 +29,20 @@ namespace Service.CatalogWrite.Endpoints.Endpoints.Books
 	/// <param name="sender">Mediator request sender.</param>
 	public sealed class GetBookByIdEndpoint(ISender sender) : EndpointBaseAsync
 		.WithRequest<Guid>
-		.WithActionResult//<AuthorDto>
-	{// TODO implement it
+		.WithActionResult<BookDto>
+	{
 		[AllowAnonymous]
 		[HttpGet(BookRoutes.GetById, Name = nameof(GetBookByIdEndpoint))]
-		//[ProducesResponseType(typeof(AuthorDto), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(BookDto), StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ApiVersion("1.0")]
 		[SwaggerOperation(
 			Summary = "Gets the book by id.",
 			Description = "Gets the book with the specified identifier.",
 			Tags = new[] { BookRoutes.Tag })]
-		public override async Task<ActionResult> HandleAsync([FromQuery] Guid bookId,
-																	CancellationToken cancellationToken = default) =>
-			//await sender.Send(new GetAuthorByIdQuery(new BookId(bookId)), cancellationToken)
-			//	.Match(Ok, this.HandleFailure);
-			throw new NotImplementedException();
+		public override async Task<ActionResult<BookDto>> HandleAsync([FromQuery] Guid bookId,
+															CancellationToken cancellationToken = default)
+			=> await sender.Send(new GetBookByIdQuery(new BookId(bookId)), cancellationToken)
+				.Match(Ok, this.HandleFailure);
 	}
 }
